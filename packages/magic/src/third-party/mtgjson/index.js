d4e3b9f947873731a44faeb853083ec0c6b99b7c
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2021-12-05 17:48:05 -0600

    node-fetch => fetch-h2

--- packages/magic/src/third-party/mtgjson/index.js
+++ packages/magic/src/third-party/mtgjson/index.js
@@ -1,42 +1,34 @@
-import fetch from 'node-fetch';
+import { fetch } from 'fetch-h2';
 
 import { API_PATH } from './constants.js';
 
 /**
  * Get MTGJSON 'atomic' data
  */
-export const getAtomicData = async (type) => {
-    const data = await fetch(`${API_PATH}Atomic${type}.json`)
-        .then(res => res.json());
-    return data.data;
+export const getAtomicData = async type => {
+  const data = await fetch(`${API_PATH}Atomic${type}.json`).then(res => res.json());
+  return data.data;
 };
 
 /**
  * Get MTGJSON set data
  */
-export const getSetCatalog = async (catalog) => {
-    if (!catalog) catalog = await fetch(`${API_PATH}SetList.json`)
-        .then(res => res.json());
-    return [...new Set(catalog.data)]
-        .map(obj => ({
-            object: 'set',
-            id: obj.code,
-            name: obj.name,
-            date: obj.releaseDate,
-            type: obj.type,
-            size: obj?.baseSetSize || obj?.totalSetSize
-        }));
-}
+export const getSetCatalog = async catalog => {
+  if (!catalog) catalog = await fetch(`${API_PATH}SetList.json`).then(res => res.json());
+  return [...new Set(catalog.data)].map(obj => ({
+    object: 'set',
+    id: obj.code,
+    name: obj.name,
+    date: obj.releaseDate,
+    type: obj.type,
+    size: obj?.baseSetSize || obj?.totalSetSize,
+  }));
+};
 /**
  * Filter MTGJSON set data
  */
 export const filterSetObjects = (set_catalog, card_catalog) => {
-    return set_catalog
-        .filter(obj =>
-            [...new Set(
-                card_catalog
-                    .map(obj => obj.printings)
-                    .flat(1)
-            )].includes(obj.id)
-        );
-}
+  return set_catalog.filter(obj =>
+    [...new Set(card_catalog.map(obj => obj.printings).flat(1))].includes(obj.id)
+  );
+};
--- packages/magic/src/third-party/scryfall/catalog.js
+++ packages/magic/src/third-party/scryfall/catalog.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+import { fetch } from 'fetch-h2';
 
 import { API_PATH, ignoredLayouts, ignoredSetTypes } from './constants.js';
 
@@ -6,27 +6,33 @@ import { API_PATH, ignoredLayouts, ignoredSetTypes } from './constants.js';
  * Fetches bulk cards data from Scryfall.
  */
 export const fetchBulkData = async (type, catalog_uri) => {
-    // Get Scryfall oracle catalog data
-    const catalog = await fetch(catalog_uri || `${API_PATH}bulk-data/${type}`).then(res => res.json());
-    // Get oracle card data
-    const data = await fetch(catalog.download_uri).then(res => res.json());
-    return data;
-}
+  // Get Scryfall oracle catalog data
+  const catalog = await fetch(catalog_uri || `${API_PATH}bulk-data/${type}`).then(res =>
+    res.json()
+  );
+  // Get oracle card data
+  const data = await fetch(catalog.download_uri).then(res => res.json());
+  return data;
+};
 
 /**
  * Filters non-sanctioned card entries and null card properties.
  */
-export const filterCatalog = (data) => {
-    return data
-        .filter(card => 
-            // Exclude extraneous card layouts and set types from non-sanctioned formats
-            !ignoredLayouts.includes(card.layout) &&
-            !ignoredSetTypes.includes(card.set_type) &&
-            // Exclude new cards from previews / etc not yet legal but will be in the future.
-            !(card.legalities.future == 'legal' &&
-                Object.values(card.legalities).filter(x => x == 'legal').length == 1) &&
-            // Remove null card object placeholders for back-halves of double-faced cards.
-            !(card.layout != 'normal' && !card.oracle_text && !card.power && !card.loyalty)
-        // Alphabetical sort data by cardname
-        ).sort((a, b) => a.name.localeCompare(b.name));
-}
+export const filterCatalog = data => {
+  return data
+    .filter(
+      card =>
+        // Exclude extraneous card layouts and set types from non-sanctioned formats
+        !ignoredLayouts.includes(card.layout) &&
+        !ignoredSetTypes.includes(card.set_type) &&
+        // Exclude new cards from previews / etc not yet legal but will be in the future.
+        !(
+          card.legalities.future == 'legal' &&
+          Object.values(card.legalities).filter(x => x == 'legal').length == 1
+        ) &&
+        // Remove null card object placeholders for back-halves of double-faced cards.
+        !(card.layout != 'normal' && !card.oracle_text && !card.power && !card.loyalty)
+      // Alphabetical sort data by cardname
+    )
+    .sort((a, b) => a.name.localeCompare(b.name));
+};
