22117c8e5a50d4b210044b76c01f8c37c89162f4
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2021-12-16 08:27:20 -0600

    Prep work for cards scraper db functions | @services/mtgo-scraper

--- services/mtgo-scraper/src/scraper/index.js
+++ services/mtgo-scraper/src/scraper/index.js
@@ -1,72 +1,82 @@
 import chalk from 'chalk';
 
-import { parseTime, CLI_CLEAR_CONSOLE } from '@packages/cli';
+import { getArgs, parseTime, CLI_CLEAR_CONSOLE, CLI_PAUSE } from '@packages/cli';
 import { setDelay } from '@packages/database';
 
 import { generateEventURIs, addEventEntry, updateEventEntry, checkDatabaseEntries } from './../utils/database';
 import { getDates } from './../utils/dates';
+
 import usePuppeteerStealth from './puppeteer';
+import { formatCardsCollection } from './scrape-cards';
 import { scrapeWotCEvent, scrapeGoldfishEvent } from './scrape-event';
+
+const fs = require('fs');
 
 const run = async (args) => {
   try {
-    // Create date range.
-    const dates = getDates(args);
-
-    // Get incomplete and complete event results from results table.
-    const { incomplete, complete } = await checkDatabaseEntries(dates, true);
-
-    // Create URIs for event queue.
-    const queue = generateEventURIs(dates)
-      // Skip URI if event entry already exists.
-      .filter(uri => !complete.includes(uri));
-
     // Setup Puppeteer
-    const { browser, page } = await usePuppeteerStealth();
+    const { browser, page } = await usePuppeteerStealth({ headless: true });
 
-    // Fetch events synchronously to avoid request timeout.
-    CLI_CLEAR_CONSOLE();
-    console.log('>> Scraping WotC Events...');
-    const startTime = Date.now();
-    for (let i = 0; i < queue.length; i++) {
-      await setDelay(500);
-      const uri = queue[i];
+    if (getArgs(args, ['-c', '--cards'], 0)) {
+      await CLI_PAUSE();
+      const data = await formatCardsCollection({ page });
+      fs.writeFileSync('tags_output.json', JSON.stringify(data));
+    } else {
+      // Create date range.
+      const dates = getDates(args);
 
-      // Output current progress to console.
+      // Get incomplete and complete event results from results table.
+      const { incomplete, complete } = await checkDatabaseEntries(dates, true);
+
+      // Create URIs for event queue.
+      const queue = generateEventURIs(dates)
+        // Skip URI if event entry already exists.
+        .filter(uri => !complete.includes(uri));
+
+      // Fetch events synchronously to avoid request timeout.
       CLI_CLEAR_CONSOLE();
-      const progress = (((i + 1) / queue.length) * 100).toFixed(2);
-      console.log(`>> Scraping '${chalk.yellow(uri)}'...`);
-      console.log(`   Progress: ${progress}% (${i + 1}/${queue.length}) complete.`);
-      const queueRate = (Date.now() - startTime) / (1000 * (i + 1)); // in seconds
-      if (i) console.log(`   About ${parseTime((queue.length - (i + 1)) * queueRate)} remaining.\n`);
-      else console.log('   Calculating time remaining...\n');
+      console.log('>> Scraping WotC Events...');
+      const startTime = Date.now();
+      for (let i = 0; i < queue.length; i++) {
+        await setDelay(500);
+        const uri = queue[i];
 
-      // Event exists but is missing third-party archetype labels.
-      if (incomplete.map(obj => obj.uri).includes(uri)) {
-        console.log(`   ${chalk.yellow('Note')}: This database entry already exists, but is`);
-        console.log(`         missing third-party archetype labels.\n`);
-        // Deconstruct event uri.
-        const format = uri.split('-')[0];
-        const type = uri.split('-').slice(1,-3).join('-');
-        // Find event uid.
-        const { event: uid } = incomplete.filter(obj => obj.uri == uri)[0];
-        // Update only Goldfish archetype.
-        const goldfishData = await scrapeGoldfishEvent(page, format, type, uid);
-        if (!goldfishData) continue;
-        await updateEventEntry(null, null, goldfishData);
-      } else {
-        // Scrape WotC and Goldfish page.
-        const { players, ...event } = await scrapeWotCEvent(uri) || {};
-        if (JSON.stringify(event) === '{}')  {
-          console.log(`   This event does not exist.\n>  Skipping event...`);
-          continue;
+        // Output current progress to console.
+        CLI_CLEAR_CONSOLE();
+        const progress = (((i + 1) / queue.length) * 100).toFixed(2);
+        console.log(`>> Scraping '${chalk.yellow(uri)}'...`);
+        console.log(`   Progress: ${progress}% (${i + 1}/${queue.length}) complete.`);
+        const queueRate = (Date.now() - startTime) / (1000 * (i + 1)); // in seconds
+        if (i) console.log(`   About ${parseTime((queue.length - (i + 1)) * queueRate)} remaining.\n`);
+        else console.log('   Calculating time remaining...\n');
+
+        // Event exists but is missing third-party archetype labels.
+        if (incomplete.map(obj => obj.uri).includes(uri)) {
+          console.log(`   ${chalk.yellow('Note')}: This database entry already exists, but is`);
+          console.log(`         missing third-party archetype labels.\n`);
+          // Deconstruct event uri.
+          const format = uri.split('-')[0];
+          const type = uri.split('-').slice(1,-3).join('-');
+          // Find event uid.
+          const { event: uid } = incomplete.filter(obj => obj.uri == uri)[0];
+          // Update only Goldfish archetype.
+          const goldfishData = await scrapeGoldfishEvent(page, format, type, uid);
+          if (!goldfishData) continue;
+          await updateEventEntry(null, null, goldfishData);
+        } else {
+          // Scrape WotC and Goldfish page.
+          const { players, ...event } = await scrapeWotCEvent(uri) || {};
+          if (JSON.stringify(event) === '{}')  {
+            console.log(`   This event does not exist.\n>  Skipping event...`);
+            continue;
+          }
+          const goldfishData = await scrapeGoldfishEvent(page, event.format, event.type, event.uid);
+          // Create new database entry.
+          await addEventEntry(players, event, goldfishData);
         }
-        const goldfishData = await scrapeGoldfishEvent(page, event.format, event.type, event.uid);
-        // Create new database entry.
-        await addEventEntry(players, event, goldfishData);
       }
     }
-
+    
     // Cleanup
     await page.close();
     await browser.close();
