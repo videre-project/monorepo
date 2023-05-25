91d93a5464574ba745255849f9540450bc2fbbd9
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-05-25 09:02:07 -0500

    [packages/@videre-database] Remove package

--- packages/database/src/constants.js
+++
@@ -1,4 +0,0 @@
-/**
- * Number of milliseconds in a day.
- */
-export const day = 8.64 * 10**7;
--- packages/database/src/prisma/index.js
+++
@@ -1,6 +0,0 @@
-// import { PrismaClient } from '@prisma/client'
-
-// /**
-//  * Creates a new prisma client instance.
-//  */
-// export const prisma = new PrismaClient();
