77df9866088761f8bf69bc6927ec9b2d197b4016
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2023-05-25 09:01:08 -0500

    [packages/@videre-querybuilder] Remove package

--- packages/querybuilder/src/parameters/events/index.js
+++
@@ -1,2 +0,0 @@
-export * from './formats.js';
-export * from './types.js';
--- packages/querybuilder/src/parameters/index.js
+++
@@ -1,9 +0,0 @@
-// Globals
-export * from './date-range.js';
-export * from './uids.js';
-
-// For event query
-export * as events from './events';
-
-// Express parameter parsing
-export * as express from './express';
