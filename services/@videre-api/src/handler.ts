3a81125748521a5b5cc66d5d6608a2667e7fbebf
Cory Bennett <32466081+Qonfused@users.noreply.github.com>
2024-01-11 08:35:14 -0600

    [services/@videre-api] Set 10s request timeout

--- services/@videre-api/src/handler.ts
+++ services/@videre-api/src/handler.ts
@@ -10,6 +10,11 @@ import type { Sql } from './db/postgres';
 import type Env from './env';
 
 
+/**
+ * Maximum request execution time in milliseconds
+ */
+const MAX_TIMEOUT = 10_000;
+
 export interface Context {
   cf: ExecutionContext;
   params: { [key: string]: any };
@@ -17,17 +22,23 @@ export interface Context {
 }
 
 export default (req: Request, ctx: Context, env: Env): Promise<Response> =>
-  router
-    // Catch-all for any other requests
-    .all('*', () => error(404))
-    // Pass Cloudflare provided arguments to the router
-    .handle(req, ctx, env)
-    // Handle any response transformations
-    .then((res => {
-      if (res instanceof Response) return res;
-      return json({
-        parameters: ctx.params,
-        data: res
-      });
-    }))
-    .catch(error);
+  new Promise((resolve) => {
+    // Set a maximum request time of 10 seconds
+    setTimeout(() => resolve(error(408, 'Request timed out')), MAX_TIMEOUT);
+
+    router
+      // Catch-all for any other requests
+      .all('*', () => error(404))
+      // Pass Cloudflare provided arguments to the router
+      .handle(req, ctx, env)
+      // Handle any response transformations
+      .then((res => {
+        if (res instanceof Response) return res;
+        return json({
+          parameters: ctx.params,
+          data: res
+        });
+      }))
+      .catch(error)
+      .then(resolve);
+  });
